2d4e063e8cdfaafb7548eda994ad03653522109f
Mihaly Hunyady <mihaly_hunyady@epam.com>
2017-09-05 15:13:35 +0200

    Initial commit

---
+++ Assets/Scripts/CardGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardGenerator : MonoBehaviour {
+
+	public GameObject card;
+
+	// Use this for initialization
+	void Start () {
+		
+		//card.AddComponent<Rigidbody>();
+		card.AddComponent<MeshFilter>();
+		card.AddComponent<BoxCollider>();
+		card.AddComponent<MeshRenderer>();	
+
+		for (int i = 0; i < 5; i++) {
+			card.transform.position = new Vector3 (2.8f + i,0, 7.24f);		
+		//	card.transform.rotation = new Quaternion (0f, 0.1f * i, 0f);
+			Instantiate (card);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
