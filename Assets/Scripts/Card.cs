aef13915d16bbb4dbde1db4ef6448b7268e1410b
Mihaly Hunyady <mihaly_hunyady@epam.com>
2017-09-06 15:19:56 +0200

    add texture to cubes

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+namespace Cards
+{
+	public class Card
+	{
+		private GameObject card;
+		public Card (Vector3 position, String pictureName)
+		{ 
+			card = GameObject.CreatePrimitive (PrimitiveType.Cube);
+			card.name = "Card" + position.x+position.y+position.z;
+			Texture2D tex = Resources.Load(pictureName) as Texture2D;	
+			card.GetComponent<Renderer>().material.mainTexture = tex;
+			card.transform.position = position;
+			//card.transform.localScale = new Vector3 (1.5f, 1f, 0.01f);
+
+		}
+
+		public void rotate(Vector3 rotationMatrix){
+			card.transform.Rotate (rotationMatrix * Time.deltaTime, Space.World);
+		}
+	}
+}
+
--- Assets/Scripts/CardGenerator.cs
+++ Assets/Scripts/CardGenerator.cs
@@ -2,21 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+namespace Cards{
+
 public class CardGenerator : MonoBehaviour {
 
-	public List<GameObject> cards;
+		public List<Card> cards;
 	// Use this for initialization
 	void Start () {
+			cards = new List<Card> ();
 		QualitySettings.antiAliasing = 2;
 		//card.AddComponent<Rigidbody>();
 	
-		for (int i = 0; i < 5; i++) {
-			var card = GameObject.CreatePrimitive (PrimitiveType.Cube);
-			cards.Add (card);
-			card.name="Card "+i;					
-			card.transform.position = new Vector3 (0f + (i*2f),-8, 10);	
-			card.transform.localScale = new Vector3 (1.5f, 1f, 0.01f);
-
+		for (int i = 0; i < 10; i++) {
+			var card = new Card (new Vector3 (0f + (i*2f),-8, 10),"nyuszi");		
+				cards.Add (card);
 		//	card.transform.rotation = new Quaternion (0f, 0.1f * i, 0f);
 		}
 	}
@@ -24,7 +23,8 @@ public class CardGenerator : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		for(int i = 0; i< cards.Count;i++){
-			cards[i].transform.Rotate(new Vector3(0,10,0) * Time.deltaTime, Space.World);
+				cards[i].rotate(new Vector3(0,40,0));
 		}
 	}
+}
 }
