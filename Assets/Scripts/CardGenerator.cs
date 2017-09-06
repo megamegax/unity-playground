e52994dc4cea593200e3cf811964013b4ef31d62
Mihaly Hunyady <mihaly_hunyady@epam.com>
2017-09-06 16:11:44 +0200

    add multiple rows and columns to card generation

--- Assets/Scripts/CardGenerator.cs
+++ Assets/Scripts/CardGenerator.cs
@@ -12,10 +12,18 @@ public class CardGenerator : MonoBehaviour {
 			cards = new List<Card> ();
 		QualitySettings.antiAliasing = 2;
 		//card.AddComponent<Rigidbody>();
-	
-		for (int i = 0; i < 10; i++) {
-			var card = new Card (new Vector3 (0f + (i*2f),-8, 10),"nyuszi");		
+			int row = 0;
+			int maxColumn = 5;
+			int column = 0;
+		for (int i = 0; i < 25; i++) {
+				var card = new Card (new Vector3 (-2.5f + (column*2f),(row*1.5f) + -8, 10),"nyuszi");		
 				cards.Add (card);
+				if (maxColumn == column) {
+					column = 0;
+					row++;
+				} else {
+					column++;
+				}
 		//	card.transform.rotation = new Quaternion (0f, 0.1f * i, 0f);
 		}
 	}
