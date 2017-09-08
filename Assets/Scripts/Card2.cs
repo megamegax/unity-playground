8af8050c82bfb41b90d59b00d66d560c2b018109
Mihaly Hunyady <mihaly_hunyady@epam.com>
2017-09-08 11:32:46 +0200

    use a sample as main menu

---
+++ Assets/Scripts/Card2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cards
+{
+	public class Card2: MonoBehaviour {
+		public GameObject card;
+		private bool rotate = false;
+		void Start () {
+			CardboardMagnetSensor.SetEnabled (true);
+		}
+		void Update(){
+			if(CardboardMagnetSensor.CheckIfWasClicked() || GvrControllerInput.IsTouching || Input.anyKeyDown){
+			//if (isCameraWatching()) {
+				//rotate = true;
+				card.transform.Rotate (new Vector3(0,40,0) * Time.deltaTime, Space.World);
+			}
+		
+			if (rotate) {
+				//card.transform.Rotate (new Vector3(0,40,0) * Time.deltaTime, Space.World);
+			}
+		}
+		private bool isCameraWatching(){
+			RaycastHit hit;
+			GameObject hittedObject = null;
+			Camera camera = Camera.current;
+			Vector3 cameraCenter = camera.ScreenToWorldPoint (new Vector3(Screen.width / 2, Screen.height / 2, camera.nearClipPlane));
+			Debug.DrawLine (cameraCenter, transform.forward);
+			if (Physics.Raycast (cameraCenter, transform.forward,out hit,100)) {
+				hittedObject = hit.transform.gameObject;
+			}
+			return hit.point.Equals(card.transform.position);
+		}
+	}
+}
